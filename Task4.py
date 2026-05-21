e14a7bd9e86b1132c50e963ebd23f5fc99252c3c
Rohit <rohits78009@gmail.com>
2026-05-21 21:36:27 +0530

    task4 done

---
+++ Task4.py
@@ -0,0 +1,9 @@
+# Program to calculate the area of a circle
+
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * radius * radius
+
+print("Area of the circle =", round(area, 2))
