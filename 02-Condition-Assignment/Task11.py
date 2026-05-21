9e451f5067bd4ebaf61a97e0e710bb8c3ba1dba2
Rohit <rohits78009@gmail.com>
2026-05-21 18:48:42 +0530

    task11 done

---
+++ 02-Condition-Assignment/Task11.py
@@ -0,0 +1,8 @@
+username1="user1"
+username1_password1="pass@123"
+username2=input("Enter your username ")
+username2_password2=input("Enter your password ")
+if username1 == username2 and username1_password1 == username2_password2:
+    print("You are valid user")
+else:
+    print("you are not a valid user")    
