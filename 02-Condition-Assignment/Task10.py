18f5fb2d38565cd6d6bc800b6aae82733a29c405
Rohit <rohits78009@gmail.com>
2026-05-21 17:45:41 +0530

    task10 done

---
+++ 02-Condition-Assignment/Task10.py
@@ -0,0 +1,16 @@
+marks=int(input("Enter your marks :"))
+if marks >=90 and marks <=100:
+    print("your Grade is A")
+elif marks >= 80 and marks <=89:
+    print("your Grade is B")
+elif marks >=70 and marks <=79:
+    print("Your Grade is C")
+elif marks >=60 and marks <=69:
+    print("Your Grade is D")
+elif marks >=50 and marks <=59:
+    print("Your grade is E")
+elif marks >=0 and marks <=49:
+    print("Your Grade is F")
+else:
+    print("invalid Marks Enter")                        
+    
