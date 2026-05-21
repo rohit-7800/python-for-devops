84329cd5ca2a4877419ee36d5899e311c31e450c
Rohit <rohits78009@gmail.com>
2026-05-21 21:53:16 +0530

    task8 done

---
+++ Task8.py
@@ -0,0 +1,8 @@
+# Validating Email Domain
+
+email = input("Enter your email address: ")
+
+if "gmail.com" in email:
+    print("Email is eligible for registration.")
+else:
+    print("Email is not eligible for registration.")
